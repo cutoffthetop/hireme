1223726c081000ef42a580881c9f8d2002d91c0b
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2014-02-09 15:12:06 +0100

    Add docstring, fix template param name

--- src/hireme/tasks/__init__.py
+++ src/hireme/tasks/__init__.py
@@ -5,11 +5,14 @@ from flask import request
 
 
 def render_task(func):
+    """Decorator for task solving functions. Provides raw form data from the
+    request and expects a string formatted return value."""
+
     def rendered():
         params = dict(title=func.__module__.split('.')[-1] or '')
         if request.method == 'POST':
             input_data = request.form.get('input')
-            params['input_data'] = input_data
+            params['input'] = input_data
             params['solution'] = func(input_data)
         return render_template('task.html', **params)
 
