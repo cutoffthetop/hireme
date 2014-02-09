44fc43903860b22154556b2596138c073b2ce103
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2014-02-09 15:12:32 +0100

    Add docstrings

--- src/hireme/server.py
+++ src/hireme/server.py
@@ -6,10 +6,12 @@ from tasks import task1, task2
 
 
 def render_index():
+    """Render the index page template."""
     return flask.render_template('index.html', title='index')
 
 
 def app_factory():
+    """Create a new Flask instance and configure the URL map."""
     app = flask.Flask(import_name=__package__)
     app.add_url_rule('/', 'index', render_index)
     app.add_url_rule('/task1', 'task1', task1.solve, methods=['GET', 'POST'])
@@ -18,6 +20,7 @@ def app_factory():
 
 
 def run_local(*args, **kwargs):
+    """Run the app on a local development server with debugging enabled."""
     app = app_factory()
     app.debug = True
     app.run()
