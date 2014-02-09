8b03c265938fba3b31b0353065af7acfe268fc1f
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2014-02-09 19:32:47 +0100

    Add dummy test functions

---
+++ src/hireme/testsuite/test_rendering.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def test_render_index():
+    assert True
+
+
+def test_render_task():
+    assert True
--- src/hireme/testsuite/test_server.py
+++ src/hireme/testsuite/test_server.py
@@ -1,2 +1,9 @@
-def test_lol():
+# -*- coding: utf-8 -*-
+
+
+def test_app_factory():
+    assert True
+
+
+def test_run_local():
     assert True
