00cdcaf3152c8b6add5832856a01d05e6dafe5b3
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2014-02-10 17:37:14 +0100

    Use assertion context manager

--- src/hireme/testsuite/test_task1.py
+++ src/hireme/testsuite/test_task1.py
@@ -9,42 +9,37 @@ from werkzeug.exceptions import BadRequest
 from ..task1 import solve
 from . import ctx_setter
 
-# TODO: Reduce boilerplate with test generators.
-
 
 @ctx_setter(method='POST', data={'input': ''})
 def test_too_few_lines():
-    with assert_raises(BadRequest):
-        expected = 'You need to enter at least 3 lines.'
-        assert expected in solve().exception.message
-
-
-@ctx_setter(method='POST', data={'input': ''})
-def test_too_few_chars():
-    with assert_raises(BadRequest):
-        expected = 'You need to enter 0 < k < 200000 chars in line 1.'
-        assert expected in solve().exception.message
+    expected = 'You need to enter at least 3 lines.'
+    with assert_raises(BadRequest) as cm:
+        solve()
+    assert expected in cm.exception.description
 
 
 @ctx_setter(method='POST', data={'input': 'A' * 200001 + '\n1\nA'})
 def test_too_many_chars():
-    with assert_raises(BadRequest):
-        expected = 'You need to enter 0 < k < 200000 chars in line 1.'
-        assert expected in solve().exception.message
+    expected = 'You need to enter 0 < k < 200000 chars in line 1.'
+    with assert_raises(BadRequest) as cm:
+        solve()
+    assert expected in cm.exception.description
 
 
 @ctx_setter(method='POST', data={'input': 'A\nA\nA'})
 def test_non_int_query_lenght():
-    with assert_raises(BadRequest):
-        expected = 'The second line must be an integer.'
-        assert expected in solve().exception.message
+    expected = 'The second line must be an integer.'
+    with assert_raises(BadRequest) as cm:
+        solve()
+    assert expected in cm.exception.description
 
 
 @ctx_setter(method='POST', data={'input': 'A\n2\nA'})
 def test_query_too_short():
-    with assert_raises(BadRequest):
-        expected = 'Less words were provided than specified.'
-        assert expected in solve().exception.message
+    expected = 'Less words were provided than specified.'
+    with assert_raises(BadRequest) as cm:
+        solve()
+    assert expected in cm.exception.description
 
 
 @ctx_setter(method='POST', data={'input': 'A B C\n1\nD'})
